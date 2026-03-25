057f11c19df7d8259241ffc7a01c1bc1e33e52c1
amrit-GH23 <amritkumar2005btw@gmail.com>
2026-03-25 19:16:30 +0530

    intialised frontend

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl font-bold text-center mt-4'>Welcome to the Chat App</h1>
+    </>
+  )
+}
+
+export default App
